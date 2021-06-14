0b88814ee403d4d80aec6940f8896a96e0f310c8
kaidey <zezepinto@hotmail.com>
2021-06-14 12:22:58 +0100

    Traffic light spawns and color animation complete

--- src/js/game/app.js
+++ src/js/game/app.js
@@ -5,202 +5,296 @@ import { moon } from "./moon.js";
 import { ground } from "./ground.js";
 import { road } from "./road.js";
 import { clouds } from "./clouds.js";
+import { trafficLight } from "./trafficLight.js";
+import { math } from "./math.js";
 
 import { RoomEnvironment } from "https://cdn.skypack.dev/three/examples/jsm/environments/RoomEnvironment.js";
 import { OrbitControls } from "https://cdn.skypack.dev/three/examples/jsm/controls/OrbitControls.js";
+import { Clock } from "../../node_modules/three/build/three.module.js";
+
+const SEPARATION_DISTANCE = 15;
 
 class CarRacingGame {
-  constructor() {
-    this.cameraMode = 1;
+	constructor() {
+		this.cameraMode = 1;
+		this.lights = [];
+		this.unused = [];
+		this.separationDistance_ = SEPARATION_DISTANCE;
 
-    this.initialize();
-    this.initInput();
-  }
+		this.initialize();
+		this.initInput();
+	}
 
-  initialize() {
-    const container = document.getElementById("container");
-    this.renderer = new THREE.WebGLRenderer({ antialias: true });
-    this.renderer.setPixelRatio(window.devicePixelRatio);
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.renderer.outputEncoding = THREE.sRGBEncoding;
-    this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
-    this.renderer.toneMappingExposure = 0.85;
-    container.appendChild(this.renderer.domElement);
+	initialize() {
+		this.clock = new Clock();
 
-    window.addEventListener(
-      "resize",
-      () => {
-        this.onWindowResize();
-      },
-      false
-    );
+		const container = document.getElementById("container");
+		this.renderer = new THREE.WebGLRenderer({ antialias: true });
+		this.renderer.setPixelRatio(window.devicePixelRatio);
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		this.renderer.outputEncoding = THREE.sRGBEncoding;
+		this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
+		this.renderer.toneMappingExposure = 0.85;
 
-    this.initCameras();
+		this.renderer.setAnimationLoop(() => {
+			this.lights.forEach((tl) => {
+				tl.tick(this.clock.getDelta());
+			});
+			this.renderer.render(this.scene, this.mainCamera);
+		});
 
-    this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0x1668a6);
-    this.scene.fog = new THREE.Fog(0x000000, 500, 10000);
+		container.appendChild(this.renderer.domElement);
 
-    // Adiciona todos os objetos necessários à cena
-    this.moon = new moon.Moon({
-      scene: this.scene,
-    });
+		window.addEventListener(
+			"resize",
+			() => {
+				this.onWindowResize();
+			},
+			false
+		);
 
-    this.ground = new ground.Ground({ scene: this.scene });
+		this.initCameras();
 
-    this.road = new road.Road({
-      scene: this.scene,
-    });
+		this.scene = new THREE.Scene();
+		this.scene.background = new THREE.Color(0x1668a6);
+		this.scene.fog = new THREE.Fog(0x000000, 500, 10000);
 
-    this.clouds = new clouds.Clouds({
-      scene: this.scene,
-      camera: this.mainCamera,
-    });
+		// Adiciona todos os objetos necessários à cena
+		this.moon = new moon.Moon({
+			scene: this.scene,
+		});
 
-    this.car = new car.Car({
-      scene: this.scene,
-    });
+		this.ground = new ground.Ground({ scene: this.scene });
 
-    // raf == RequestAnimationFrame
-    // Esta função define como a aplicação se vai comportar a cada frame
-    this.raf();
-    this.onWindowResize();
-  }
+		this.road = new road.Road({
+			scene: this.scene,
+		});
 
-  onWindowResize() {
-    this.mainCamera.aspect = window.innerWidth / window.innerHeight;
-    this.mainCamera.updateProjectionMatrix();
+		this.clouds = new clouds.Clouds({
+			scene: this.scene,
+			camera: this.mainCamera,
+		});
 
-    this.OrthographicCamera.aspect = window.innerWidth / window.innerHeight;
-    this.OrthographicCamera.updateProjectionMatrix();
+		this.car = new car.Car({
+			scene: this.scene,
+		});
 
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-  }
+		/* this.tl = new trafficLight.TrafficLight({
+			scene: this.scene,
+		}); */
 
-  // Inicializa as câmeras e define as suas posições e para onde devem olhar
-  initCameras() {
-    this.PerspectiveCamera = new THREE.PerspectiveCamera(
-      40,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      400
-    );
+		// raf == RequestAnimationFrame
+		// Esta função define como a aplicação se vai comportar a cada frame
+		this.raf();
+		this.onWindowResize();
+	}
 
-    this.PerspectiveCamera.position.set(12, 3, 3);
-    this.PerspectiveCamera.lookAt(-10, 0, -7);
+	LastLightPosition() {
+		if (this.lights.length == 0) {
+			return this.car.position.z - SEPARATION_DISTANCE;
+		}
 
-    const size = 1;
-    const near = 0;
-    const far = 10000;
+		return this.lights[this.lights.length - 1].tlModel.position.z;
+	}
 
-    // Foi multiplicado o 2 nas componentes esquerda e direita, uma vez que o carro estava a parecer um pouco esticado na horintal
-    this.OrthographicCamera = new THREE.OrthographicCamera(
-      -size * 2,
-      size * 2,
-      size,
-      -size,
-      near,
-      far
-    );
+	SpawnLight(last) {
+		let obj = null;
 
-    this.OrthographicCamera.zoom = 0.05;
-    this.OrthographicCamera.position.set(0, 20, 0);
-    this.OrthographicCamera.lookAt(0, 0, -7);
+		if (this.unused.length > 0) {
+			obj = this.unused.pop();
+			obj.tlModel.visible = true;
+		} else {
+			obj = new trafficLight.TrafficLight({
+				scene: this.scene,
+			});
+		}
 
-    if (this.cameraMode === 0) {
-      this.mainCamera = this.PerspectiveCamera;
-    } else {
-      this.mainCamera = this.OrthographicCamera;
-    }
+		obj.tlModel.position.z =
+			last - math.rand_range(SEPARATION_DISTANCE, SEPARATION_DISTANCE * 3);
 
-    // this.controls = new OrbitControls(
-    //   this.mainCamera,
-    //   this.renderer.domElement
-    // );
-  }
+		this.lights.push(obj);
+	}
 
-  raf() {
-    requestAnimationFrame((t) => {
-      if (this.previousRaf === null) {
-        this.previousRAF_ = t;
-      }
+	UpdateLights() {
+		const invisible = [];
+		const visible = [];
 
-      this.raf();
-      this.step((t - this.previousRaf) / 1000.0);
-      this.renderer.render(this.scene, this.mainCamera);
-      this.cameraUpdate();
+		for (let obj of this.lights) {
+			let difPos = obj.tlModel.position.z - this.car.position.z;
 
-      this.previousRaf = t;
-    });
-  }
+			let redLight = obj.tlModel.getObjectByName("redLight", true);
 
-  step(timeElapsed) {
-    this.car.update(timeElapsed);
-    this.clouds.animate();
-  }
-  initInput() {
-    document.addEventListener("keypress", (e) => this.onKeyPress(e), false);
-  }
+			let currentColor = redLight.material.color;
 
-  onKeyPress(event) {
-    switch (event.code) {
-      // O utilizador quer mudar para a projeccção ortográfica
-      case "KeyO":
-        console.log("ortográfica");
-        this.mainCamera = this.OrthographicCamera;
+			if (
+				difPos < 0.5 &&
+				difPos > 0 &&
+				currentColor.equals(new THREE.Color(0xff0000))
+			) {
+				console.log("Lost");
+			} else if (difPos > 15) {
+				invisible.push(obj);
+				obj.tlModel.visible = false;
+			} else {
+				visible.push(obj);
+			}
+		}
 
-        break;
+		this.lights = visible;
+		this.unused.push(...invisible);
+	}
+	MaybeSpawn() {
+		let last = this.LastLightPosition();
 
-      // O utilizador quer mudar para a projeccção em perspetiva
-      case "KeyP":
-        console.log("Perspetiva");
-        this.mainCamera = this.PerspectiveCamera;
+		if (this.lights.length < 7) {
+			if (Math.abs(last - this.car.position.z) < this.separationDistance_) {
+				this.separationDistance_ = math.rand_range(
+					SEPARATION_DISTANCE,
+					SEPARATION_DISTANCE * 2
+				);
+				this.SpawnLight(last);
+			}
+		}
+	}
 
-        break;
-    }
-  }
+	onWindowResize() {
+		this.mainCamera.aspect = window.innerWidth / window.innerHeight;
+		this.mainCamera.updateProjectionMatrix();
 
-  cameraUpdate() {
-    // Nesta função vamos ter que atualizar as duas câmeras, de modo a que quando o utilizador der switch entre as câmeras
-    // A transição seja suave
+		this.OrthographicCamera.aspect = window.innerWidth / window.innerHeight;
+		this.OrthographicCamera.updateProjectionMatrix();
 
-    // Deslocação da câmera Perspetiva
-    // creating an offset position for camera with respect to the car
-    var offset = new THREE.Vector3(
-      this.car.position.x + 12,
-      this.car.position.y + 3,
-      this.car.position.z
-    );
-    //tried to create delay position value for enable smooth transition for camera
-    this.PerspectiveCamera.position.lerp(offset, 0.5);
-    //updating lookat alway look at the car
-    this.PerspectiveCamera.lookAt(
-      this.car.position.x - 10,
-      this.car.position.y,
-      this.car.position.z - 7
-    );
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+	}
 
-    // Deslocação da câmera Ortográfica
+	// Inicializa as câmeras e define as suas posições e para onde devem olhar
+	initCameras() {
+		this.PerspectiveCamera = new THREE.PerspectiveCamera(
+			40,
+			window.innerWidth / window.innerHeight,
+			0.1,
+			400
+		);
+		this.PerspectiveCamera.position.set(12, 3, 3);
+		this.PerspectiveCamera.lookAt(-10, 0, -7);
 
-    var offset = new THREE.Vector3(
-      this.car.position.x,
-      this.car.position.y + 20,
-      this.car.position.z
-    );
-    //tried to create delay position value for enable smooth transition for camera
-    this.OrthographicCamera.position.lerp(offset, 0.5);
-    //updating lookat alway look at the car
-    this.OrthographicCamera.lookAt(
-      this.car.position.x,
-      this.car.position.y,
-      this.car.position.z - 7
-    );
-    // this.controls.update();
-  }
+		const size = 1;
+		const near = 0;
+		const far = 10000;
+
+		// Foi multiplicado o 2 nas componentes esquerda e direita, uma vez que o carro estava a parecer um pouco esticado na horintal
+		this.OrthographicCamera = new THREE.OrthographicCamera(
+			-size * 2,
+			size * 2,
+			size,
+			-size,
+			near,
+			far
+		);
+
+		this.OrthographicCamera.zoom = 0.05;
+		this.OrthographicCamera.position.set(0, 20, 0);
+		this.OrthographicCamera.lookAt(0, 0, -7);
+
+		if (this.cameraMode === 0) {
+			this.mainCamera = this.PerspectiveCamera;
+		} else {
+			this.mainCamera = this.OrthographicCamera;
+		}
+
+		// this.controls = new OrbitControls(
+		//   this.mainCamera,
+		//   this.renderer.domElement
+		// );
+	}
+
+	func() {
+		this.MaybeSpawn();
+		this.UpdateLights();
+	}
+
+	raf() {
+		requestAnimationFrame((t) => {
+			if (this.previousRaf === null) {
+				this.previousRAF_ = t;
+			}
+			this.raf();
+			this.step((t - this.previousRaf) / 1000.0);
+			this.renderer.render(this.scene, this.mainCamera);
+			this.cameraUpdate();
+			this.func();
+
+			this.previousRaf = t;
+		});
+	}
+
+	step(timeElapsed) {
+		this.car.update(timeElapsed);
+		this.clouds.animate();
+	}
+	initInput() {
+		document.addEventListener("keypress", (e) => this.onKeyPress(e), false);
+	}
+
+	onKeyPress(event) {
+		switch (event.code) {
+			// O utilizador quer mudar para a projeccção ortográfica
+			case "KeyO":
+				console.log("ortográfica");
+				this.mainCamera = this.OrthographicCamera;
+
+				break;
+
+			// O utilizador quer mudar para a projeccção em perspetiva
+			case "KeyP":
+				console.log("Perspetiva");
+				this.mainCamera = this.PerspectiveCamera;
+
+				break;
+		}
+	}
+
+	cameraUpdate() {
+		// Nesta função vamos ter que atualizar as duas câmeras, de modo a que quando o utilizador der switch entre as câmeras
+		// A transição seja suave
+
+		// Deslocação da câmera Perspetiva
+		// creating an offset position for camera with respect to the car
+		var offset = new THREE.Vector3(
+			this.car.position.x + 12,
+			this.car.position.y + 3,
+			this.car.position.z
+		);
+		//tried to create delay position value for enable smooth transition for camera
+		this.PerspectiveCamera.position.lerp(offset, 0.5);
+		//updating lookat alway look at the car
+		this.PerspectiveCamera.lookAt(
+			this.car.position.x - 10,
+			this.car.position.y,
+			this.car.position.z - 7
+		);
+
+		// Deslocação da câmera Ortográfica
+
+		var offset = new THREE.Vector3(
+			this.car.position.x,
+			this.car.position.y + 20,
+			this.car.position.z
+		);
+		//tried to create delay position value for enable smooth transition for camera
+		this.OrthographicCamera.position.lerp(offset, 0.5);
+		//updating lookat alway look at the car
+		this.OrthographicCamera.lookAt(
+			this.car.position.x,
+			this.car.position.y,
+			this.car.position.z - 7
+		);
+		// this.controls.update();
+	}
 }
 
 let _APP = null;
 
 window.addEventListener("DOMContentLoaded", () => {
-  _APP = new CarRacingGame();
+	_APP = new CarRacingGame();
 });
---
+++ src/js/game/trafficLight.js
@@ -0,0 +1,133 @@
+import * as THREE from "https://cdn.skypack.dev/three";
+import {
+	AnimationClip,
+	AnimationMixer,
+	NumberKeyframeTrack,
+} from "../../node_modules/three/build/three.module.js";
+
+export const trafficLight = (() => {
+	class TrafficLight {
+		constructor(params) {
+			this.params = params;
+			this.currentLight = 0;
+			this.loadModel();
+			this.initAnimation();
+
+			this.lightBox_ = this.tlModel.getObjectByName("lightBox");
+		}
+
+		initAnimation() {
+			const times = [0, 5, 8, 13];
+			const valuesGreen = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+			const valuesYellow = [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0];
+			const valuesRed = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0];
+
+			const greenKF = new NumberKeyframeTrack(
+				".material.color",
+				times,
+				valuesGreen,
+				THREE.InterpolateDiscrete
+			);
+			const yellowKF = new NumberKeyframeTrack(
+				".material.color",
+				times,
+				valuesYellow,
+				THREE.InterpolateDiscrete
+			);
+			const redKF = new NumberKeyframeTrack(
+				".material.color",
+				times,
+				valuesRed,
+				THREE.InterpolateDiscrete
+			);
+
+			const greenClip = new AnimationClip("green", -1, [greenKF]);
+			const yellowClip = new AnimationClip("yellow", -1, [yellowKF]);
+			const redClip = new AnimationClip("red", -1, [redKF]);
+
+			this.greenMixer = new AnimationMixer(
+				this.tlModel.getObjectByName("greenLight", true)
+			);
+			this.yellowMixer = new AnimationMixer(
+				this.tlModel.getObjectByName("yellowLight", true)
+			);
+			this.redMixer = new AnimationMixer(
+				this.tlModel.getObjectByName("redLight", true)
+			);
+
+			const greenAction = this.greenMixer.clipAction(greenClip);
+			const yellowAction = this.yellowMixer.clipAction(yellowClip);
+			const redAction = this.redMixer.clipAction(redClip);
+
+			greenAction.play();
+			yellowAction.play();
+			redAction.play();
+		}
+
+		loadModel() {
+			const poleGeometry = new THREE.CylinderGeometry(0.1, 0.1, 5);
+			const lightBoxGeometry = new THREE.BoxGeometry(0.3, 1, 0.5);
+
+			const structureMaterial = new THREE.MeshBasicMaterial({
+				color: 0xa9a9a9,
+			});
+
+			const pole = new THREE.Mesh(poleGeometry, structureMaterial);
+			const lightBox = new THREE.Mesh(lightBoxGeometry, structureMaterial);
+			lightBox.name = "lightBox";
+
+			const redLight = new Light({
+				name: "redLight",
+				y: -0.3,
+			}).lightMesh;
+
+			const yellowLight = new Light({
+				name: "yellowLight",
+				y: 0,
+			}).lightMesh;
+
+			const greenLight = new Light({
+				name: "greenLight",
+				y: 0.3,
+			}).lightMesh;
+
+			greenLight.material.color.setHex(0x00ff00);
+
+			lightBox.add(redLight);
+			lightBox.add(yellowLight);
+			lightBox.add(greenLight);
+
+			pole.add(lightBox);
+			lightBox.position.y = 3;
+
+			pole.position.x = -3;
+
+			this.tlModel = pole;
+
+			this.params.scene.add(this.tlModel);
+		}
+
+		tick(delta) {
+			this.greenMixer.update(delta);
+			this.yellowMixer.update(delta);
+			this.redMixer.update(delta);
+		}
+	}
+
+	class Light {
+		constructor(params) {
+			this.lightMesh = new THREE.Mesh(
+				new THREE.CircleGeometry(0.1, 20),
+				new THREE.MeshBasicMaterial({ color: 0x000000 })
+			);
+
+			this.lightMesh.rotation.y = Math.PI / 2;
+			this.lightMesh.position.x = 0.2;
+			this.lightMesh.position.y = params.y;
+			this.lightMesh.name = params.name;
+		}
+	}
+	return {
+		TrafficLight: TrafficLight,
+	};
+})();
